dd963cbd810805b85648a079c29fbe33b719b2cb
joellundell <joel.lundell@gmail.com>
2013-01-15 16:37:51 +0100

    Added accounthistory

---
+++ fr.isep.mobilemoney/src/fr/isep/mobilemoney/AccountHistory.java
@@ -0,0 +1,22 @@
+package fr.isep.mobilemoney;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AccountHistory extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account_history);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_account_history, menu);
+		return true;
+	}
+
+}
