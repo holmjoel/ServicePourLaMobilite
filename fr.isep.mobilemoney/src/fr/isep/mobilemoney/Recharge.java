bedbed3431cbb10cdb0871da72bc37253461dd63
Guillermo De los Santos García <memo.dlsg@gmail.com>
2013-01-16 11:04:46 +0100

    Added Manage Acount, non-working Recharge

--- fr.isep.mobilemoney/src/fr/isep/mobilemoney/Recharge.java
+++ fr.isep.mobilemoney/src/fr/isep/mobilemoney/Recharge.java
@@ -1,26 +1,22 @@
 package fr.isep.mobilemoney;
 
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import fr.isep.jsonobjects.ResponseJSON;
 
-public class MainActivity extends MoneyActivity {
+public class Recharge extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_recharge);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_recharge, menu);
 		return true;
-	}
-	
-	public void showResult(ResponseJSON resp){
-		//Not applicable
 	}
 
 }
